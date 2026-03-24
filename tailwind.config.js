f4bb2de343ca569bf6a5f9da9f2df210893e70e3
suitedigitale <hello@scalaabile.it>
2026-03-24 13:36:54 +0100

    upload
    git add .
    git commit -m "final build tailwind"
    git push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
